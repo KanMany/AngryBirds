e1f56662d4c3878c365be9f500b6b50de697e958
KanMany <75095033+KanMany@users.noreply.github.com>
2021-01-06 13:20:55 +0000

    Add files via upload

---
+++ BaseClass.js
@@ -0,0 +1,25 @@
+class BaseClass {
+    constructor(x, y, width, height) {
+        var options = {
+            density: 1.5,
+            restitution: 1,
+            friction: 0.4
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.image = loadImage("sprites/base.png");
+        World.add(world, this.body);
+    }
+    display() {
+        var pos = this.body.position;
+        var angle = this.body.angle;
+
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        imageMode(CENTER)
+        image(this.image, 0, 0, this.width, this.height);
+        pop();
+    }
+}
